69a65c30ff4895f39969a063fa0b8f55eedc5412
Dylan Davies <dylandaviddavies@gmail.com>
2022-06-25 19:45:51 -0400

    Add container component

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { ContainerModule } from '../../components/container/container.module';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule, DashboardRoutingModule, ContainerModule],
 })
 export class DashboardModule {}
